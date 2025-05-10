0f22738c08865ab800839453024f8422f990d82c
Lucas <natanaelzigaran@gmail.com>
2025-05-10 10:46:08 -0300

    Casi se finaliza con la tarea, hay errores en el codigo por solucionar

--- Main.js
+++ Main.js
@@ -1,8 +1,9 @@
 const cambio = 1000; //Valor del Dolar
 
 const history = []
-
+// -----------------------------paso 1: realizar la funcion del menu----------------------------------------
 function menu() {
+    // Prompt se le pregunta al usuario
     const opcion = prompt(
 
         "1. Convertir Pesos a Dolar"+
@@ -10,7 +11,7 @@ function menu() {
         "2. Salir"
     );    
 
-    
+    //Switch para que ingrese el valor de opcion
     switch (opcion) {
         case "1":
     
@@ -26,5 +27,31 @@ function menu() {
             menu();
     };
 };
-
 menu();
+//---------------------------------Este es el paso 2 se pide el monto a convertir-------------------------------------
+function conversion() {
+    const input = prompt ("Ingrese su dinero en pesos");
+
+    const montoPeso = parseFloat(input); //ParseFloat para que el monto sea solamente numerico
+}
+/// paso 3 La operacion de convercion
+const montoUsd = montoPeso / cambio;
+
+const operation = {
+        date: new Date().toLocaleString(),
+        from: "ARS",
+        to: "USD",
+        montoPeso: montoPeso,
+        montoUsd: montoUsd.toFixed(2), 
+        rate: cambio
+};
+//---------------------------Esto es para guardar la conversion al array de history
+history.push(operation); 
+
+    
+    alert(
+        `✅ ${montoPeso} ARS = ${montoUsd.toFixed(2)} USD\n` +
+        `(Tasa: 1 USD = ${EXCHANGE_RATE} ARS)`
+    );
+
+    menu();
